71c3cacbae447cc40521d004820d13767d86c0ae
sophiaww-w <sophiaw021910@gmail.com>
2026-04-17 19:35:48 -0400

    Add organization data for We Love People

---
+++ data.js
@@ -0,0 +1,14 @@
+const orgData = [
+  {
+    id: "nyc1",
+    name: "We Love People",
+    city: "New York",
+    country: "USA",
+    lat: 40.7128,
+    lng: -74.0060,
+    goal: 1000,
+    raised: 420,
+    description: "Providing meals and support for homeless people.",
+    stripe: "https://buy.stripe.com/test_yourlink"
+  }
+];
